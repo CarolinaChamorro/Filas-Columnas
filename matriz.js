c74fe362e7cc64a7bd1f160e4953f62ffe28c117
CarolinaChamorro <52305952+CarolinaChamorro@users.noreply.github.com>
2019-07-24 21:00:13 -0500

    Update matriz.js

--- matriz.js
+++ matriz.js
@@ -1,18 +1,18 @@
+// imprime las columnas
 function columnas() {
-    for (i = 0; i < matriz.length; i++) {
-        for (j = 0; j < matriz[1].length; j++) {
-            console.log(matriz[i][j]);
+    for (let i = 0; i < matriz.length; i++) {
+        for (let k = 0; k < matriz[i].length; k++) {
+            console.log(matriz[i][k]);
         }
     }
 }
-
+// imprime las filas
 function filas(){
     for(i=0;i<matriz.length;i++){
         console.log(matriz[i]);
     }
 }
-
+imprime toda la matriz
 function todo(){
     console.log(matriz);
 }
-todo(matriz)
